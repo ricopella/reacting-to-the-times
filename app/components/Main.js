1d6bb5c2e69833ac7184b9c8647fd4a4b66f0864
Narin Sundarabhaya <nrs710@gmail.com>
2017-09-17 23:10:01 -0700

    beautified main

--- app/components/Main.js
+++ app/components/Main.js
@@ -14,22 +14,21 @@ class Main extends React.Component {
         return (
           <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="# ">Reacting to the NY Times</a>
-  
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
+              <a className="navbar-brand" href="# ">Reacting to the NY Times</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link" id="nav-heading" href="#heading">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" id="nav-articles" href="#results">Articles</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" id="nav-scrape" href="#results">New Articles</a>
-              </li>
+                <li className="nav-item active">
+                  <a className="nav-link" id="nav-heading" href="#heading">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" id="nav-articles" href="#results">Articles</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" id="nav-scrape" href="#results">New Articles</a>
+                </li>
               </ul>
             </div>
           </nav>
@@ -39,14 +38,14 @@ class Main extends React.Component {
             <div className="intro-text">
             <h1>Welcome to Reacting to the NY Times!</h1>
             <p>A fresh brew of news from NYTimes.com</p>
-          </div>
+            </div>
           </div>
         </body>
 
         <div className="row-fluid footer">
           <div className="col-12 text-center">
-          <p> 2017 &copy; Narin R. Sundarabhaya</p>
-        </div>
+            <p> 2017 &copy; Narin R. Sundarabhaya</p>
+          </div>
         </div>
       </div>
         );
