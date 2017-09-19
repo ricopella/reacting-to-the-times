eb04ecdc2ae0a6160c87901861d8f2f7701feff5
Narin Sundarabhaya <nrs710@gmail.com>
2017-09-19 16:03:46 -0700

    started helper

--- src/components/children/Search.js
+++ src/components/children/Search.js
@@ -11,82 +11,84 @@ class Search extends Component {
 
     return (
       <section id="searc-results">
-        <div className="jumbotron">
-          <h1 className="text-center">
-            <strong>
-              <i className="fa fa-newspaper-o"></i>
-              New York Times Search</strong>
-          </h1>
-        </div>
+        <div className="container">
+          <div className="jumbotron">
+            <h1 className="text-center">
+              <strong>
+                <i className="fa fa-newspaper-o"></i>
+                New York Times Search</strong>
+            </h1>
+          </div>
 
-        <div className="row">
-          <div className="col-sm-12">
-            <br/>
-            <div className="panel panel-primary">
-              <div className="panel-heading">
-                <h3 className="panel-title">
-                  <strong>
-                    <i className="fa  fa-list-alt"></i>
-                    Search Parameters</strong>
-                </h3>
-              </div>
-              <div className="panel-body">
+          <div className="row">
+            <div className="col-sm-12">
+              <br/>
+              <div className="panel panel-primary">
+                <div className="panel-heading">
+                  <h3 className="panel-title">
+                    <strong>
+                      <i className="fa  fa-list-alt"></i>
+                      Search Parameters</strong>
+                  </h3>
+                </div>
+                <div className="panel-body">
 
-                <form role="form">
+                  <form role="form">
 
-                  <div className="form-group">
-                    <label for="search">Search Term:</label>
-                    <input type="text" className="form-control" id="search-term"/>
-                  </div>
+                    <div className="form-group">
+                      <label >Search Term:</label>
+                      <input type="text" className="form-control" id="search-term"/>
+                    </div>
 
-                  <div className="form-group">
-                    <label for="pwd">Number of Records to Retrieve:</label>
-                    <select className="form-control" id="num-records-select">
-                      <option value="1">1</option>
+                    <div className="form-group">
+                      <label >Number of Records to Retrieve:</label>
+                      <select className="form-control" id="num-records-select">
+                        <option value="1">1</option>
 
-                      <option value="5" selected>5</option>
-                      <option value="10">10</option>
-                    </select>
-                  </div>
+                        <option value="5" selected>5</option>
+                        <option value="10">10</option>
+                      </select>
+                    </div>
 
-                  <div className="form-group">
-                    <label for="start-year">Start Year (Optional):</label>
-                    <input type="text" className="form-control" id="start-year"/>
-                  </div>
+                    <div className="form-group">
+                      <label >Start Year (Optional):</label>
+                      <input type="text" className="form-control" id="start-year"/>
+                    </div>
 
-                  <div className="form-group">
-                    <label for="end-year">End Year (Optional):</label>
-                    <input type="text" className="form-control" id="end-year"/>
-                  </div>
+                    <div className="form-group">
+                      <label >End Year (Optional):</label>
+                      <input type="text" className="form-control" id="end-year"/>
+                    </div>
 
-                  <button type="submit" className="btn btn-default" id="run-search">
-                    <i className="fa fa-search"></i>
-                    Search</button>
-                  <button type="button" className="btn btn-default" id="clear-all">
-                    <i className="fa fa-trash"></i>
-                    Clear Results</button>
+                    <button type="submit" className="btn btn-default" id="run-search">
+                      <i className="fa fa-search"></i>
+                      Search</button>
+                    <button type="button" className="btn btn-default" id="clear-all">
+                      <i className="fa fa-trash"></i>
+                      Clear Results</button>
 
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="row">
-          <div className="col-sm-12">
-            <br/>
+          <div className="row">
+            <div className="col-sm-12">
+              <br/>
 
-            <div className="panel panel-primary">
+              <div className="panel panel-primary">
 
-              <div className="panel-heading">
-                <h3 className="panel-title">
-                  <strong>
-                    <i className="fa fa-table"></i>
-                    Top Articles</strong>
-                </h3>
+                <div className="panel-heading">
+                  <h3 className="panel-title">
+                    <strong>
+                      <i className="fa fa-table"></i>
+                      Top Articles</strong>
+                  </h3>
+                </div>
+
+                <div className="panel-body" id="well-section"></div>
               </div>
-
-              <div className="panel-body" id="well-section"></div>
             </div>
           </div>
         </div>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,21 +2,23 @@ import axios from "axios";
 
 const helpers = {
 
-    //   runQuery: (location) => {
+    runQuery: (search) => {
 
-    //     console.log(location);
+        console.log(search);
 
-    //     // Figure out the geolocation
-    //     const queryURL = "http://api.opencagedata.com/geocode/v1/json?query=" + location + "&pretty=1&key=" + geocodeAPI;
+        // Figure out the geolocation
+        const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff5954" +
+                "7cd47b931d8cbc56b:0:74623931&q=" + search;
 
-    //     return axios.get(queryURL).then((response) => {
+        return axios
+            .get(queryURL)
+            .then((response) => {
 
-    //       console.log(response);
-    //       return response.data.results[0].formatted;
-    //     });
+                console.log(response);
+                return response.data.results[0].formatted;
+            });
 
-    //   }
+    }
 };
-
 
 export default helpers;
