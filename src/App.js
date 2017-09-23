9390d29635a9bb54f8a0725a0d203871a3e025e2
Narin Sundarabhaya <nrs710@gmail.com>
2017-09-22 20:25:16 -0700

    added switch and nomatch back to heading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Nav from "./components/children/Nav";
 import Heading from "./components/children/Heading";
 import Footer from "./components/children/Footer";
 import Search from "./components/children/Search";
 import Saved from "./components/children/Saved";
 
-const App = () =>
-  <Router >
-    <div>
-      <Nav />
-        <Route exact path="/" component={Heading} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/saved" component={Saved} />
-      <Footer />
-    </div>
-  </Router>;
+const App = () => <Router >
+  <div>
+    <Nav/>
+    <Switch>
+      <Route exact path="/" component={Heading}/>
+      <Route exact path="/search" component={Search}/>
+      <Route exact path="/saved" component={Saved}/>
+      <Route component={Heading}/>
+    </Switch>
+    <Footer/>
+  </div>
+</Router>;
 
 export default App;
-
