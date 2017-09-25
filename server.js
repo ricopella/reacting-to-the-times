c00df042d7e99475cb2cac543137cd4127d14997
Narin Sundarabhaya <nrs710@gmail.com>
2017-09-24 23:58:23 -0700

    moved client side files to client directory

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express'),
     logger = require("morgan"),
     mongoose = require("mongoose"),
     routes = require("./routes"),
+    mognoURL = require("./keys"),
     app = express(),
     port = process.env.PORT || 3000;
 
@@ -11,13 +12,12 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json({type: "application/vnd.api+json"}));
-app.use(express.static("public"));
+app.use(express.static("client/public"));
 
 app.use(routes);
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = Promise;
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactingtimes", {useMongoClient: true});
-
+mongoose.connect(mognoURL);
 app.use((req, res) => res.status(404).send("Sorry can't find that!"));
 app.listen(port, () => console.log(`==> 🌎  Listening on PORT ${port}. Visit http://localhost:${port}`.green));
